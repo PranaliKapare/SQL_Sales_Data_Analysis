c2e62c1dfbcd02413aa174e86ad2759905e8de3b
PranaliKapare <166198774+PranaliKapare@users.noreply.github.com>
2026-02-24 12:13:04 +0530

    Create Insert_Data.sql

---
+++ Insert_Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers VALUES(1, 'Amit', 'Pune'),(2, 'Sneha', 'Mumbai'),(3, 'Rahul', 'Delhi'),(4, 'Priya', 'Pune');
+
+INSERT INTO products VALUES(101, 'Laptop', 50000),(102, 'Mobile', 20000),(103, 'Headphones', 2000);
+
+INSERT INTO orders VALUES(1, 1, 101, 1, '2024-01-10'),(2, 2, 102, 2, '2024-01-15'),(3, 1, 103, 3, '2024-02-05'),(4, 3, 101, 1, '2024-02-20'),(5, 4, 102, 1, '2024-03-01');
